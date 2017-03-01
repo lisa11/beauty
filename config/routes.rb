f04dabc49938305c83ebe5b24cd16d0eb42b1356
Raghu Betina <raghu@firstdraft.com>
2017-03-01 23:05:20 +0000

    Generate products resource with starter:resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  # Routes for the Product resource:
+  # CREATE
+  get "/products/new", :controller => "products", :action => "new"
+  post "/create_product", :controller => "products", :action => "create"
+
+  # READ
+  get "/products", :controller => "products", :action => "index"
+  get "/products/:id", :controller => "products", :action => "show"
+
+  # UPDATE
+  get "/products/:id/edit", :controller => "products", :action => "edit"
+  post "/update_product/:id", :controller => "products", :action => "update"
+
+  # DELETE
+  get "/delete_product/:id", :controller => "products", :action => "destroy"
+  #------------------------------
+
   devise_for :users
   # Routes for the User resource:
   # READ
